fe2e0d8f4d2f0a904fc34c8fa0642e70756ad7ee
saradab-mindfire <saradab@mindfiresolutions.com>
2024-06-02 23:46:06 +0530

    chore: created shared modules for few libraries

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'preps',
+    loadChildren: () => import("./preparation/preparation.module").then(m => m.PreparationModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
